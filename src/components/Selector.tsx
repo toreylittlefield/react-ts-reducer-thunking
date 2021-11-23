820265b03ba3583d4c98a66545b34ba71bc1ed5e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-23 18:24:49 +0800

    move to own component

---
+++ src/components/Selector.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { teams } from '../teamnames';
+
+type SelectorProps = {
+  list: typeof teams;
+  selectedTeam: typeof teams[0];
+  handleSelectTeam: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+export const Selector = ({ list, selectedTeam, handleSelectTeam }: SelectorProps) => {
+  if (!Array.isArray(list)) return null;
+  return (
+    <>
+      <label htmlFor="team-select">Select Team</label>
+      <select name="teams" id="team-select" value={selectedTeam?.name} onChange={handleSelectTeam}>
+        {list.length > 10 ? <option value="all">Show All Leagues</option> : null}
+        {list.map((team) => (
+          <option key={team.id} value={team.name}>
+            {team.name}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};

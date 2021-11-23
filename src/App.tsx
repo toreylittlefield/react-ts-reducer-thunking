8b35f76b72accd55f0609571233f5de510816ee0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-23 19:36:20 +0800

    move useThunkReducer and reducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,69 +3,10 @@ import './styles.css';
 import { League, LeagueInfo } from './components/LeagueInfo';
 import { Selector } from './components/Selector';
 import { teams } from './teamnames';
+import { Cache, ActionType, useThunkReducer } from './hooks/useThunkReducer';
 
 const endpoint = `https://api-football-standings.azharimm.site/leagues
 `;
-
-type Cache<T> = { [url: string]: T };
-
-interface ApiInterface {
-  status: boolean;
-  data: League[] | League;
-}
-
-type ActionType =
-  | { type: 'LOADING' }
-  | { type: 'FETCH_LEAGUES'; payload: ApiInterface }
-  | { type: 'FETCH_LEAGUE_BY_ID'; payload: ApiInterface }
-  | { type: 'ERROR'; payload: string }
-  | ((dispatch: React.Dispatch<ActionType>) => void);
-
-type StateType = {
-  loading: boolean;
-  response: null | ApiInterface;
-  error: null | string;
-  cache: Cache<string>;
-};
-
-const initialState: StateType = {
-  loading: false,
-  response: null,
-  error: null,
-  cache: {},
-};
-
-const reducer = (state: StateType, action: ActionType) => {
-  if (typeof action === 'function') return state;
-  switch (action.type) {
-    case 'LOADING':
-      return { ...state, loading: true };
-    case 'FETCH_LEAGUES':
-      return { ...state, loading: false, response: action.payload };
-    case 'FETCH_LEAGUE_BY_ID':
-      return { ...state, loading: false, response: action.payload };
-    case 'ERROR':
-      return { ...state, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
-
-function useFetch(): [StateType, React.Dispatch<ActionType>] {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const enhancedDispatch = useCallback(
-    (action: ActionType) => {
-      if (typeof action === 'function') {
-        action(dispatch);
-      } else {
-        dispatch(action);
-      }
-    },
-    [dispatch]
-  );
-  return [state, enhancedDispatch];
-}
 
 const fetchLeagues = (params: string, cache: Cache<any>) => {
   return (dispatch: React.Dispatch<ActionType>) => {
@@ -104,7 +45,7 @@ const fetchByLeagueId = (params: string, cache: Cache<any>) => {
 const intialSelectedState = { id: '', abbr: '', name: '' };
 
 export default function App() {
-  const [{ loading, response, error, cache }, dispatch] = useFetch();
+  const [{ loading, response, error, cache }, dispatch] = useThunkReducer();
   const [list, setList] = useState<typeof teams>([]);
   const [selectedTeam, setSelectedTeam] = useState<typeof teams[0]>(intialSelectedState);
 
---
+++ src/hooks/useThunkReducer.tsx
@@ -0,0 +1,62 @@
+import { useCallback, useReducer } from 'react';
+import { League } from '../components/LeagueInfo';
+
+export type Cache<T> = { [url: string]: T };
+
+interface ApiInterface {
+  status: boolean;
+  data: League[] | League;
+}
+
+export type ActionType =
+  | { type: 'LOADING' }
+  | { type: 'FETCH_LEAGUES'; payload: ApiInterface }
+  | { type: 'FETCH_LEAGUE_BY_ID'; payload: ApiInterface }
+  | { type: 'ERROR'; payload: string }
+  | ((dispatch: React.Dispatch<ActionType>) => void);
+
+type StateType = {
+  loading: boolean;
+  response: null | ApiInterface;
+  error: null | string;
+  cache: Cache<string>;
+};
+
+const initialState: StateType = {
+  loading: false,
+  response: null,
+  error: null,
+  cache: {},
+};
+
+const reducer = (state: StateType, action: ActionType) => {
+  if (typeof action === 'function') return state;
+  switch (action.type) {
+    case 'LOADING':
+      return { ...state, loading: true };
+    case 'FETCH_LEAGUES':
+      return { ...state, loading: false, response: action.payload };
+    case 'FETCH_LEAGUE_BY_ID':
+      return { ...state, loading: false, response: action.payload };
+    case 'ERROR':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export function useThunkReducer(): [StateType, React.Dispatch<ActionType>] {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const enhancedDispatch = useCallback(
+    (action: ActionType) => {
+      if (typeof action === 'function') {
+        action(dispatch);
+      } else {
+        dispatch(action);
+      }
+    },
+    [dispatch]
+  );
+  return [state, enhancedDispatch];
+}

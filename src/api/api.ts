b849f8b5208c0d8fef70fe60212e687bf0fe2a96
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-24 12:36:47 +0800

    add the cache for fetches

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,20 +1,25 @@
-import { Cache, ActionType, ApiDispatchTypes } from '../hooks/useThunkReducer';
+import { Cache, ActionType } from '../hooks/useThunkReducer';
 
 const endpoint = `https://api-football-standings.azharimm.site/leagues`;
+
+export type ApiDispatchTypes = 'FETCH_LEAGUES' | 'FETCH_LEAGUE_BY_ID' | 'FETCH_LEAGUE_STANDINGS';
 
 export const dispatchFetch = (cache: Cache, type: ApiDispatchTypes, params: string = '') => {
   return (dispatch: React.Dispatch<ActionType>) => {
     dispatch({ type: 'LOADING' });
     params = endpoint + params;
     if (params in cache) {
-      dispatch({ type: type, payload: cache[params] });
+      const cached = cache[params];
+      dispatch({ type: type, payload: cached });
       return;
     }
     fetch(params)
       .then((res) => res.json())
       .then((json) => {
-        cache[params] = json;
-        dispatch({ type: type, payload: json });
+        const res = json;
+        res.type = type;
+        cache[params] = res;
+        dispatch({ type: type, payload: res });
       })
       .catch((error) => dispatch({ type: 'ERROR', payload: error }));
   };

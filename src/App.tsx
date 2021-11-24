aa57d028d8aa9913a0f26997c01ea506eee8f7e6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-24 12:36:27 +0800

    add fetch to league stats / standings but having problems with type errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { League, LeagueInfo } from './components/LeagueInfo';
 import { Selector } from './components/Selector';
 import { teams } from './teamnames';
 import { useThunkReducer } from './hooks/useThunkReducer';
-import { dispatchFetch } from './api/api';
+import { ApiDispatchTypes, dispatchFetch } from './api/api';
 
 const intialSelectedState = { id: '', abbr: '', name: '' };
 
@@ -15,15 +15,21 @@ export default function App() {
 
   useEffect(() => {
     if (selectedTeam.id === '') {
-      const action = 'FETCH_LEAGUES';
+      const action: ApiDispatchTypes = 'FETCH_LEAGUES';
       const fetchAllLeagues = dispatchFetch(cache, action);
       dispatch(fetchAllLeagues);
     }
     if (selectedTeam.id.length !== 0) {
-      const action = 'FETCH_LEAGUE_BY_ID';
+      const action: ApiDispatchTypes = 'FETCH_LEAGUE_BY_ID';
       const param = `/${selectedTeam.id}`;
       const fetchLeague = dispatchFetch(cache, action, param);
       dispatch(fetchLeague);
+      if (list.length === 1) {
+        const action: ApiDispatchTypes = 'FETCH_LEAGUE_STANDINGS';
+        const param = `/${selectedTeam.id}/standings?season=2021&sort=asc`;
+        const fetchLeague = dispatchFetch(cache, action, param);
+        dispatch(fetchLeague);
+      }
     }
   }, [dispatch, selectedTeam.id, cache]);
 
@@ -110,7 +116,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <form>
+      <form onSubmitCapture={(e) => e.preventDefault()}>
         <fieldset>
           <legend>Debouncer Example</legend>
           <label htmlFor="search"> Search Leagues</label>
@@ -125,12 +131,12 @@ export default function App() {
           <Selector list={list} selectedTeam={selectedTeam} handleSelectTeam={handleSelectTeam} />
         </fieldset>
       </form>
-      {Array.isArray(response.data) ? (
-        response.data.map((league) => <LeagueInfo key={league.id} league={league} />)
-      ) : (
-        <LeagueInfo league={response.data} />
-      )}
-      <div>{JSON.stringify({ loading, response, error }, null, 2)}</div>
+      {response.type === 'FETCH_LEAGUES' &&
+        response.data.map((league) => <LeagueInfo key={league.id} league={league} />)}
+      {response.type === 'FETCH_LEAGUE_BY_ID' && <LeagueInfo league={response.data} />}
+      {response.type === 'FETCH_LEAGUE_STANDINGS' && <LeagueInfo league={response.data} />}
+
+      {response.type === 'FETCH_LEAGUE_STANDINGS' && <div>{JSON.stringify({ loading, response, error }, null, 2)}</div>}
     </div>
   );
 }
